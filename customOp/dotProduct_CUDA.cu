a583065f82fbacaa01a22caedad988c845d0bab7
DiSoJi <diegosolisj@gmail.com>
2019-05-24 18:38:06 -0600

    fixes

--- customOp/dotProduct_CUDA.cu
+++ customOp/dotProduct_CUDA.cu
@@ -12,7 +12,7 @@ void dotProdKernel(int *A, int *B, int *C, int N){
 }
 
 void dotProd_CPU(int *A, int *B, int *C, int N){
-    for (int i = 0; int < N; i++){
+    for (int i = 0; i < N; i++){
         *C += A[i] * B[i];
     }
 }
@@ -73,7 +73,7 @@ int main() {
     clock_t end = clock();
     double time = (double)(end-start)/CLOCKS_PER_SEC;
     printf("Time it took on CPU: %f", time);
-    
+
     //Free GPU memory
     cudaFree(first_gpu);
     cudaFree(second_gpu);
